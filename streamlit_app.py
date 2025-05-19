bf9c373dc85c7286efa46cbe081909f59fff07c3
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-05-19 11:41:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -158,7 +158,7 @@ if uploaded_files:
         st.dataframe(consolidado_virus)
         
         # Gráfico de barras
-        st.bar_chart(consolidado_virus)
+        st.bar_chart(consolidado_virus, stack=False)
     
     with tab3:
         st.header("Dados detalhados de todos os casos")
